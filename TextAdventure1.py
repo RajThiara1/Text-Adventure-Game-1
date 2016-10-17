7f7e3e2e36bd5a56d97b6f6567212892409c18f5
caleyz <caleyz@coventry.ac.uk>
2016-10-17 10:03:17 +0100

    First!

---
+++ TextAdventure1.py
@@ -0,0 +1,18 @@
+print("----------121COM Week 2 Lab Exercise 7----------")
+print("----------------<InsertTitleHere>---------------")
+print("---------------<InsertAuthorsHere>--------------")
+print("\n")
+
+print("You awake in a dark room.  Do you:")
+print("a) Scream for help.")
+print("b) Press the light switch")
+x = input("Enter a or b: ")
+if x == "a":
+    print("Someone hears your screams...")
+    # Continue adventure Here
+elif x == "b":
+    print("The light comes on.")
+    print("You do not recognise the room but you have a really bad feeling...")
+    # Continue adventure Here
+else:
+    print("That was not an option.  Game Over")
